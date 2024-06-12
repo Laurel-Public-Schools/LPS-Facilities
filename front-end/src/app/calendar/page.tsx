66406cb15ade0f6bcb529ecec174df24d580820d
biohackerellie <epkerns@gmail.com>
2024-06-12 13:25:05 -0600

    feat: Add dynamic variable for force-dynamic in calendar page

--- front-end/src/app/calendar/page.tsx
+++ front-end/src/app/calendar/page.tsx
@@ -4,6 +4,7 @@ import CalendarMain from "@/components/calendar/Calendar";
 import LoadingScreen from "@/components/ui/loadingScreen";
 import { GetAllEvents } from "@/functions/events/googleAPI";
 
+export const dynamic = "force-dynamic";
 export default function Page() {
   return (
     <div className="space-y-7">
