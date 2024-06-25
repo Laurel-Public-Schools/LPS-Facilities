9573c8bc3e916f184151b8f310f1bd6f69039bdd
biohackerellie <epkerns@gmail.com>
2024-06-25 09:44:47 -0600

    fix: another quick fix

--- front-end/src/app/calendar/layout.tsx
+++ front-end/src/app/calendar/layout.tsx
@@ -19,18 +19,18 @@ export default function calendarLayout({
         </Suspense>
       </div>
       <div className="hidden space-y-6 p-10 pb-16 sm:block lg:p-2">
-        <div className="space-y-0.5">
-          <h1 className="text-2xl font-bold">Calendar</h1>
-        </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="-mx-4 lg:w-1/5">
-            {/* <Suspense fallback={<Skeleton className="h-auto w-auto" />}> */}
-            <SidebarSearchParamsNav items={buildingSideBar2} />
-            {/* </Suspense> */}
-          </aside>
-          <div className="flex-1 lg:max-w-2xl">{children}</div>
-        </div>
+        <Suspense fallback={<Skeleton className="h-auto w-auto" />}>
+          <div className="space-y-0.5">
+            <h1 className="text-2xl font-bold">Calendar</h1>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="-mx-4 lg:w-1/5">
+              <SidebarSearchParamsNav items={buildingSideBar2} />
+            </aside>
+            <div className="flex-1 lg:max-w-2xl">{children}</div>
+          </div>
+        </Suspense>
       </div>
     </div>
   );
